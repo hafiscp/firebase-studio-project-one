bb2789a21877d98176a6695812e18f0a1caab8ec
Hafis CP <cphafis3@gmail.com>
2025-11-25 07:54:27 +0000

    line and icon div needs to be align vertically on one another (_for elem

--- src/components/community-timeline.tsx
+++ src/components/community-timeline.tsx
@@ -15,17 +15,17 @@ export function CommunityTimeline({ items }: CommunityTimelineProps) {
   return (
     <div className="relative pl-8">
       {/* The vertical line */}
-      <div className="absolute left-3.5 top-0 h-full w-0.5 bg-border"></div>
+      <div className="absolute left-3 top-0 h-full w-0.5 bg-border translate-x-px"></div>
       
       <div className="space-y-8">
         {items.map((item) => (
           <div key={item.id} className="relative flex items-start">
             {/* Dot on the timeline */}
-            <div className="absolute left-0 top-1.5 flex h-7 w-7 items-center justify-center rounded-full bg-primary">
+            <div className="absolute left-0 top-1.5 flex h-6 w-6 items-center justify-center rounded-full bg-primary">
               <Users className="h-4 w-4 text-primary-foreground" />
             </div>
 
-            <Card className="ml-8 w-full">
+            <Card className="ml-12 w-full">
               <CardHeader>
                 <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
                     <CardTitle>{item.communityName}</CardTitle>
